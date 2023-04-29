d312eb37c82c76d9d603c7b8bbb53a6bed8e0363
George Serelis <geosere@gmail.com>
2023-04-29 15:49:00 +0300

    Linear Regression Code
    First version of the basic linear regression using least squares method.
    
    Signed-off-by: George Serelis <geosere@gmail.com>

--- linear_calc.py
+++ linear_calc.py
@@ -1,32 +1,38 @@
-import datetime
-
 PATH = 'Linear_Model.txt'
 
+## Parse a text file
+## Process the data to convert the dates to the number of year using the mean (t0) as the center
+## Store in two separate lists
 def parse_txt(path):
     dates, model = [], []
     with open(path, 'r') as f:
         next(f)
         contents = f.read()
     for line in contents.splitlines():
-        elements = line.split('\t')
+        elements = line.split('\t') 
         d = float(elements[0])
         y = float(elements[1].strip('[]'))
         dates.append(d)
         model.append(y)
+    t0 = sum(dates) / len(dates)
+    dates = [d - t0 for d in dates]
     return dates, model
-
 dates, y= parse_txt(PATH)
 
-xx, yy, sx, sy, xy = 0, 0, 0, 0, 0
-for dnum, num in zip(dates, y):
-    sx += dnum
-    sy += num
-    xx += dnum*dnum
-    yy += num*num
-    xy += num*dnum
-n = float(len(y))
+## Implementing the basic formula for Linear Regression using the least squares method to fit a straight line to a set of data points
+## Using two lists of data points and returns the slope and y-intercept of the best-fit line for the data
+def Linear_Regression(dates, y):
+    xx, yy, sx, sy, xy = 0, 0, 0, 0, 0
+    for dnum, num in zip(dates, y):
+        sx += dnum
+        sy += num
+        xx += dnum*dnum
+        yy += num*num
+        xy += num*dnum
+    n = float(len(y))
 
-a = (n*xy - sx*sy)/(n*xx - sx*sx)
-b = ((sy*xx) - (sx*xy))/(n*xx - sx*sx)
-print(a)
-print(b)
+    a = (n*xy - sx*sy)/(n*xx - sx*sx)
+    b = ((sy*xx) - (sx*xy))/(n*xx - sx*sx)
+    return a, b
+
+a, b = Linear_Regression(dates, y)
