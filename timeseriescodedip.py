34d223aa3813dc36a0ddacd76e4922caf290bb08
George Serelis <geosere@gmail.com>
2023-04-29 15:15:39 +0300

    Cleaning up the code
    Signed-off-by: George Serelis <geosere@gmail.com>

--- timeseriescodedip.py
+++ timeseriescodedip.py
@@ -1,8 +1,6 @@
 import datetime
 from matplotlib import pyplot as plt
 import numpy
-from numpy import random
-
 
 start_str = input("Enter a Starting Date using (Year-Month-Day) - format: ")
 end_str = input("Enter a Finishing Date using (Year-Month-Day) - format: ")
@@ -37,9 +35,6 @@ def linear_model(START_DATE, END_DATE, T0, A, B):
 
         START_DATE += datetime.timedelta(days = 1)
 
-    #print(dates)
-    print(w_noise)
-    print(linear_model)
     return(dates, white_noise, linear_model)
 
 dates, white_noise, model = linear_model(START_DATE, END_DATE, T0, A, B)
@@ -47,21 +42,21 @@ dates, white_noise, model = linear_model(START_DATE, END_DATE, T0, A, B)
 ## Creating a text file, and writing dates + white noise in it
 
 with open("Linear_Model.txt", "w") as file:
-    file.write("dates \t \t \t    linear_y \n")
+    file.write("dates \t \t \t    linear_y \n")                 ##Needs to be fixed 
 
     for i in range(len(dates)):
         file.write(str(dates[i]) + "\t" + str(white_noise[i]) + "\n")
 
-# ## Plotting linear model 
+## Plotting linear model 
 
-# plt.plot(dates, model, label = 'Linear Model')
-# plt.plot(dates, white_noise, label = 'White noise')
+plt.plot(dates, model, label = 'Linear Model')
+plt.plot(dates, white_noise, label = 'White noise')
 
-# font = {'family':'serif','color':'blue','size':30}
-# font1 = {'family':'serif','color':'darkred','size':20}
+font = {'family':'serif','color':'blue','size':30}
+font1 = {'family':'serif','color':'darkred','size':20}
 
-# plt.title('Linear Model', fontdict = font)
-# plt.xlabel('Dates', fontdict = font1)
-# plt.ylabel('Model (mm)', fontdict = font1)
+plt.title('Linear Model', fontdict = font)
+plt.xlabel('Dates', fontdict = font1)
+plt.ylabel('Model (mm)', fontdict = font1)
 
-# plt.show()
+plt.show()
