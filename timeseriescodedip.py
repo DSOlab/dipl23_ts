96f77e89880f000e5680da37568780b4364fb069
George Serelis <geosere@gmail.com>
2023-04-29 13:17:52 +0300

    Edits on the main code
    Signed-off-by: George Serelis <geosere@gmail.com>

--- timeseriescodedip.py
+++ timeseriescodedip.py
@@ -12,47 +12,56 @@ END_DATE = datetime.datetime.strptime(end_str, "%Y-%m-%d").date()
 
 A = 40 # mm/year
 B = 0 
+T0 = (END_DATE.year + START_DATE.year)/2
+print(T0)
 mean = 0
 std = 1 
 num_samples = 1
 
 
-def linear_model(START_DATE, END_DATE, A, B):
-    dates, linear_model, random_numb  = [], [], []
+def linear_model(START_DATE, END_DATE, T0, A, B):
+    dates, linear_model, white_noise  = [], [], []
     j = 0
 
     ## Creating year-day numbers and calculating the linear model
-    while START_DATE <= END_DATE and j < 3000:
-        #rand_numb = random.randint(-5, 5) # mm
-        ## Add white noise in additional to random
+    while START_DATE <= END_DATE and j < 10000:
         rand_numb = numpy.random.normal(mean, std, size=num_samples)
-        
-        date_numb = START_DATE.year + START_DATE.timetuple().tm_yday / 365.25
-        linear_model_calc = (date_numb - 2015) * A + B
-        rand_numb = rand_numb + linear_model_calc
 
-        random_numb.append(rand_numb)
-        dates.append(date_numb)
-        linear_model.append(linear_model_calc)
-        START_DATE += datetime.timedelta(days = 1)  ## Move the line at the end of the loop
-         
-    print(dates)
-    print(random_numb)
+        epochs = START_DATE.year + START_DATE.timetuple().tm_yday / 365.25
+        linear_y = (epochs - T0) * A + B
+        w_noise = rand_numb + linear_y
+
+        white_noise.append(w_noise)
+        dates.append(epochs)
+        linear_model.append(linear_y)
+
+        START_DATE += datetime.timedelta(days = 1)
+
+    #print(dates)
+    print(w_noise)
     print(linear_model)
-    return(dates, random_numb, linear_model)
+    return(dates, white_noise, linear_model)
 
-dates, random_numb, model = linear_model(START_DATE, END_DATE, A, B)
+dates, white_noise, model = linear_model(START_DATE, END_DATE, T0, A, B)
 
-## Plotting linear model 
+## Creating a text file, and writing dates + white noise in it
 
-plt.plot(dates, model, label = 'Linear Model')
-plt.plot(dates, random_numb, label = 'White noise')
+with open("Linear_Model.txt", "w") as file:
+    file.write("dates \t \t \t    linear_y \n")
 
-font = {'family':'serif','color':'blue','size':30}
-font1 = {'family':'serif','color':'darkred','size':20}
+    for i in range(len(dates)):
+        file.write(str(dates[i]) + "\t" + str(white_noise[i]) + "\n")
 
-plt.title('Linear Model', fontdict = font)
-plt.xlabel('Dates', fontdict = font1)
-plt.ylabel('Model (mm)', fontdict = font1)
+# ## Plotting linear model 
 
-plt.show()
+# plt.plot(dates, model, label = 'Linear Model')
+# plt.plot(dates, white_noise, label = 'White noise')
+
+# font = {'family':'serif','color':'blue','size':30}
+# font1 = {'family':'serif','color':'darkred','size':20}
+
+# plt.title('Linear Model', fontdict = font)
+# plt.xlabel('Dates', fontdict = font1)
+# plt.ylabel('Model (mm)', fontdict = font1)
+
+# plt.show()
