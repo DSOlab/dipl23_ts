2434b8218ae3a9e2f802334df6b1ccf260e40e0b
George Serelis <geosere@gmail.com>
2023-07-02 16:07:24 +0300

    Linear_ calc Update:
    Changed the way i constructed the design matrix (now it creats every row
    seperately and then it appends it to the matrix)
    Added a function that returns the solustion of the least square method ("Least_Squares()")
    Added a function that calculates the final model ("LS_solutions()")
    Added a function that calculates the Linear only model ("Linear_sol()")
    
    Main function updates:
    
    Function calls updated
    Now it plots the results (plots the data provided by the text file, the Final model
    and the linear only model on the same plot)
    
    TODO:
    Update timeseriescodedip.py adding jumps
    Then, update linear_calc.py

--- linear_calc.py
+++ linear_calc.py
@@ -1,8 +1,11 @@
 import datetime
+from matplotlib import pyplot as plt
 import numpy as np
 import math
 
 PATH = 'Linear_Model.txt'
+OMEGA1 = 2 * math.pi * 2
+OMEGA2 = 2 * math.pi * 1
 
 ## Parse a text file
 ## Process the data to convert the dates to the number of year using the mean (t0) as the center
@@ -52,31 +55,74 @@ def Linear_Regression(dates, y):
 
     return a, b
 
-def Harmonic_Regression(dates):
-    # A = np.ones(shape=(len(dates),4))
-    # dx = np.ones(shape=(1,4))
-    # y = c1*math.sin(w1*dates) + d1*math.cos(w1*dates) + c2*math.sin(w2*dates) + d2*math.cos(w2*dates)
-    omega1 = 2 * math.pi * 2
-    omega2 = 2 * math.pi * 1
-
+## Generating a design matrix based on the given time differences (each time instance (epoch) - the middle epoch), and 2 given angular differences (omega1,2):
+## The resulting design matrix named "A" has dimensions "(len(dates), number of coefficients(6))"
+## The equation being modeled is: y = a*dates + b + c1*math.sin(w1*dates) + d1*math.cos(w1*dates) + c2*math.sin(w2*dates) + d2*math.cos(w2*dates)
+def Design_Matrix(dates, OMEGA1, OMEGA2):
     A = []
-    dx = []
-
     for i in range (len(dates)):
-        A.append(math.sin(omega1*dates[i]))
-        A.append(math.cos(omega1*dates[i]))
-        A.append(math.sin(omega2*dates[i]))
-        A.append(math.cos(omega2*dates[i]))
-
+        dm_row = [                          #Creating a row containing the values of the independent variables for each date
+            dates[i],
+            1,
+            math.sin(OMEGA1 * dates[i]),
+            math.cos(OMEGA1 * dates[i]),
+            math.sin(OMEGA2 * dates[i]),
+            math.cos(OMEGA2 * dates[i])
+        ]
+        A.append(dm_row)                    #Append the constructed row to the Design Matrix (list named "A")
     A = np.array(A)
-    A = np.reshape(A,(len(dates),4))
-    print(A.shape)
+    print(A)
+
     return A
 
-dates, y = parse_txt(PATH)
-#dates = epochs2dtime(dates2epochs(dates)) #Datetime -> Epochs -> dtime
-A = Harmonic_Regression(epochs2dtime(dates2epochs(dates)))
-#a, b = Linear_Regression(dates, y)
+## Solving a linear regression problem using the least square method
+## Converting the "y" input list to a NumPy array and reshape it into a column vector ensuring that "y" is in the correct format for further computations
+def Least_Squares(A, y):
+    y = np.array(y)
+    y = np.reshape(y, (len(y),1))
 
-## for least squares:
-## https://numpy.org/doc/stable/reference/generated/numpy.linalg.lstsq.html
+    dx = np.linalg.lstsq(A, y, rcond=None)[0]
+    return dx
+
+## Given the least square solutions for the equation: y = a*dates + b + c1*math.sin(w1*dates) + d1*math.cos(w1*dates) + c2*math.sin(w2*dates) + d2*math.cos(w2*dates)
+## For each date, calculate the corresponding solution creating the final model 
+def LS_solutions(dx, dates, OMEGA1, OMEGA2):
+
+    dx = list(dx)
+    solutions = []
+
+    for i in range (len(dates)):
+        solution = dx[0]*dates[i] + dx[1] + dx[2]*math.sin(OMEGA1*dates[i]) + dx[3]*math.cos(OMEGA1*dates[i]) + dx[4]*math.sin(OMEGA2*dates[i]) + dx[5]*math.cos(OMEGA2*dates[i])
+        solutions.append(solution)
+    # print(solutions)
+
+    return solutions
+## Calculating the linear model
+def Linear_sol(a, b, dates):
+    lin_sol = []
+    for i in range (len(dates)):
+        lin_sol.append(a*dates[i] + b)
+    return lin_sol
+
+if __name__ == "__main__":
+    dates, y = parse_txt(PATH)
+    A = Design_Matrix(epochs2dtime(dates2epochs(dates)), OMEGA1, OMEGA2) #Datetime -> Epochs -> dtime
+    Final_Model = LS_solutions(Least_Squares(A, y), epochs2dtime(dates2epochs(dates)), OMEGA1, OMEGA2) 
+    a, b = Linear_Regression(epochs2dtime(dates2epochs(dates)), y)
+    Lin_Solution = Linear_sol(a, b, epochs2dtime(dates2epochs(dates)))
+
+## Plotting the original data imported from the text file (created by timeseriescodedip.py) + the created model
+
+    plt.plot(dates, y, 'o', label = 'Original Data', markersize=1)
+    plt.plot(dates, Final_Model, 'r', label = "Final Model")
+    plt.plot(dates, Lin_Solution, 'y', label = "Linear Model")
+
+    font = {'family':'serif','color':'blue','size':30}
+    font1 = {'family':'serif','color':'darkred','size':20}
+
+    plt.title('Final Model', fontdict = font)
+    plt.xlabel('Dates', fontdict = font1)
+    plt.ylabel('y - values (mm)', fontdict = font1)
+    plt.legend()
+
+    plt.show()
