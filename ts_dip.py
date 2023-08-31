0a4d3ddf7fe7379f48f53d8d0dea05c69774e451
Dimitris Anastasiou <dganastasiou@gmail.com>
2023-08-31 15:55:30 +0300

    sqrtP not working

--- ts_dip.py
+++ ts_dip.py
@@ -2,113 +2,191 @@ import datetime
 from matplotlib import pyplot as plt
 import numpy as np
 import math
+from itertools import chain
+import sys
+import statistics
 
-model_coef = [40, 0, -3, -11, -3, -11] #Coefficients of the equation being modeled y = a * x + b + c1 * math.sin(w1 * x) + d1 * math.cos(w1 * x) + c2 * math.sin(w2 * x) + d2 * math.cos(w2 * x)...
-timeframe = [datetime.datetime(2015,1,1), datetime.datetime(2019,1,1)]
-jump_list = [[datetime.datetime(2016,1,1), 60], [datetime.datetime(2017,1,1), -100]]
+model_coef = [100, 0, -30, -11, 6, 36, 50, -100] #Coefficients of the equation being modeled y = a * x + b + c1 * math.sin(w1 * x) + d1 * math.cos(w1 * x) + c2 * math.sin(w2 * x) + d2 * math.cos(w2 * x)...
+timeframe = [datetime.datetime(2015,1,1), datetime.datetime(2020,1,1)]
+jump_list = [] #datetime.datetime(2017,1,1), datetime.datetime(2018,12,12)
 w = 2 * math.pi * 2 
 w2 = 2 * math.pi * 1 
-angular_freq = [w, w2]
+angular_freq = [w2, w]
 white_noise_parameters = [0, 1]
+SIGMA0 = 1
+EPSILON = 0.006694380 
+PATH = 'tuc2.cts'
 
-def middle_epoch(timeframe):
-    t1, t2 = timeframe
-    t0 = t1 + (t2 - t1)/2
-    return t0
+## PARSING CTS FILES
+def parse(PATH):
+    t, stime, coords = [], [], []
+    with open(PATH, 'r') as fin:
+        for line in fin.readlines():
+            l = line.split()
+            if len(l) >= 15:
+                tv = (datetime.datetime.strptime(l[0]+' '+l[1],"%Y-%m-%d %H:%M:%S"))
+                coordsv = ([ float(j) for j in l[2:14] ])
+                try:
+                    at = datetime.datetime.strptime(l[14]+' '+l[15],"%Y-%m-%d %H:%M:%S.%f")
+                except:
+                    at = datetime.datetime.strptime(l[14]+' '+l[15],"%Y-%m-%d %H:%M:%S")
+                stimev = at
+            try:
+                tind = t.index(tv)
+                if stimev > stime[tind]:
+                    coords[tind] = coordsv
+                    stime[tind] = stimev
+            except:
+                t.append(tv)
+                coords.append(coordsv)
+                stime.append(stime)
+    stime = [x for _,x in sorted(zip(t,stime))]
+    coords = [x for _,x in sorted(zip(t,coords))]
+    t = sorted(t)
+    return t, stime, coords
 
-def middle_epoch_fractional(epochs):
-    t0_fractional=(epochs[-1] + epochs[0])/2
-    return t0_fractional
+def geterrors(coordinates):
+    sf, sl, sh = [], [], []
+    for flh_elements in coordinates:
+        sf.append(flh_elements[7])
+        sl.append(flh_elements[9])
+        sh.append(flh_elements[11])
+    return sf, sl, sh
 
-# Given a time-frame, the mean value and the std value
-# Calculate a white noise signal 
-def white_noise(timeframe, white_noise_parameters):
-    t1, t2 = timeframe
-    mean, std = white_noise_parameters
-    white_noise = []
-    i = 0
-    while t1 <= t2 and i < 1e-5:
-        random_number = np.random.normal(mean, std)
-        white_noise.append(random_number)
-        t1 += datetime.timedelta(days = 1)
-    return(white_noise)
+def getxyz(coordinates):
+    x, y, z = [], [], []
+    for xyz_elements in coordinates:
+        x.append(xyz_elements[0])
+        y.append(xyz_elements[2])
+        z.append(xyz_elements[4])
+    return x, y, z
+
+def mean_xyz(x, y, z):
+    xm, ym, zm = map(statistics.mean, [x, y, z])
+    return xm, ym, zm
+
+#converting Earth Centered, Earth Fixed (ECEF) x,y,z coordinates in meters to Latitude, Longitude, height using ellipsoid WGS84 
+def xyz2latlon(x, y, z):
+
+    lon = math.atan2(y, x)
+    S = math.sqrt(x*x + y*y)
+    lat_old = math.atan( z / ((1 - EPSILON) * S) )
+    div = math.sqrt(1 - EPSILON*(math.sin(lat_old)*math.sin(lat_old)))
+    Ni = (6378137/div)
+
+    j = 0
+    dx = 1 
+    lat_new = 1
+
+    while dx > 1e-12 and j < 100:
+        lat_new = math.atan((z + 0.006694380*Ni*math.sin(lat_old))/S)
+        dx = abs(lat_old - lat_new)
+        lat_old = lat_new
+        j = j + 1
+    return lat_new, lon
+
+#calculating rotation matrix
+def rotation_matrix(phi, lamda):
+    a = math.sin(phi)
+    b = math.sin(lamda)
+    c = math.cos(phi)
+    d = math.cos(lamda)
+
+    r = [ 
+        [-1*b, d, 0],
+        [-1*a*d, -1*a*b, c],
+        [c*d , c*b , a]
+        ]
+    return r
+
+def topocentric_conversion(x, y, z):
+    xm, ym, zm = mean_xyz(x, y, z)
+    lat, lon = xyz2latlon(xm, ym, zm)
+    r_matrix = rotation_matrix(lat, lon)
+    e, n, u = [], [], []
+    for xyz in zip(x, y, z):
+        difxyz =  [xyz[0] - xm, xyz[1] - ym, xyz[2] - zm]
+        i=0; e.append(difxyz[0] * r_matrix[i][0] + difxyz[1] * r_matrix[i][1] + difxyz[2] * r_matrix[i][2])
+        i=1; n.append(difxyz[0] * r_matrix[i][0] + difxyz[1] * r_matrix[i][1] + difxyz[2] * r_matrix[i][2])
+        i=2; u.append(difxyz[0] * r_matrix[i][0] + difxyz[1] * r_matrix[i][1] + difxyz[2] * r_matrix[i][2])
+    return e, n, u
+
 
 # Given two datetime instances (Starting date and finishing date) create a list
 # containing every date (as datetime object) between those instances 
-def date_calc(timeframe):
-    t1, t2 = timeframe
+def date_calc(t):
+    t1, t2 = t
     dates = []
     i = 0
     while t1 <= t2 and i < 1e-5:
         dates.append(t1)
         t1 += datetime.timedelta(days = 1)
-    return(dates)
+    return dates
 
-## Converting datetime values to epochs 
-def datetime2epochs(dates):
-    epochs = []
-    for date in dates:
-        epochs.append(date.year + date.timetuple().tm_yday / 365.25)
-    return epochs
+#Calculating middle epoch in datetime
+def middle_epoch(t):
+    t1, t2 = t[0], t[-1]
+    t0 = t1 + (t2 - t1)/2
+    return t0
 
-## Calculating the middle epoch and return a list of time differences between each epoch and the middle epoch 
-def epochs2dt(epochs):
-    dt=[]
-    t0 = middle_epoch_fractional(epochs)
-    for epoch in epochs:
-        dt.append(epoch - t0)      #Calculating and appending the differences
+# For a time instance (middle epoch) calculate the time difference in fractional years between each epoch and the middle epoch 
+# Converting the datetime objects to fractional years 
+def fractionaldt(t,t0=None):
+    if not t0:
+        t0 = middle_epoch(t)
+    dt = []
+    for epoch in t:
+        dt.append( (epoch-t0).days / 365.25 )
     return dt
 
-# Given the coefficients of a linear model (a and b) and the arguement x (= dt),
-# calculate the value of the model at a requested point using: y = a * x + b
-def linear(dt, model_coef):
-    linear_model = []
-    for x in dt:
-        lin_y =  x * model_coef[0] + model_coef[1]
-        linear_model.append(lin_y)
-    return(linear_model)
+def weights_calc(coord_errors, sigma0):
+    weights = [(1 / ce ) * sigma0 for ce in coord_errors]
+    weights_array = np.array(weights)
+    P_matrix = np.diag(weights_array)
+    return P_matrix
 
-# Given the coefficients C and D (in-phase, out-of-phase) of a harmonic signal,
-# and its angular frequency (omega), calculate the value of the harmonic signal
-# at a given epoch (t) using: y = A * sin(omega * t) + B * cos(omega * t)
-# Note: the following function calculates 2 signals, for 2 different frequencies
-#def harmonic(dt, angular_freq, model_coef):
-#    harmonic_model = []
-#    for x in dt:
-#        harmonic = model_coef[2] * math.sin(angular_freq[0] * x) + model_coef[3] * math.cos(angular_freq[0] * x)
-#        harmonic2 = model_coef[4] * math.sin(angular_freq[1] * x) + model_coef[5] * math.cos(angular_freq[1] * x)
-#        harmonic_tot = harmonic + harmonic2
-#        harmonic_model.append(harmonic_tot)
-#    return(harmonic_model)
+## Given the least square solutions for an equation: y = a*dates + b + c1*math.sin(w1*dates) + d1*math.cos(w1*dates) + c2*math.sin(w2*dates) + d2*math.cos(w2*dates) + ... + j1 + ...jν
+## For each date, calculate the corresponding solution creating the final model 
+def compute_model(dx, t, freq, jumps, white_noise=None):
+    y = []   
+    dt = fractionaldt(t)   
+    
+    for d in zip(t, dt):
+        # linear part
+        yi = dx[0]*d[1] + dx[1]
+        # harmonic part
+        offset = 2
+        for i, fr in enumerate(freq):
+            yi += dx[i*2+2] * math.sin(fr * d[1]) + dx[i*2+3] * math.cos(fr * d[1])
+        # add jumps
+        offset = 2 + len(freq)*2
+        for i, date in enumerate(jumps):
+            if d[0] >= date:
+               yi += dx[offset + i]
+        # add white noise
+        if white_noise is not None:
+            yi += np.random.normal(white_noise[0], white_noise[1])
+        # append to returned list
+        y.append(yi)
+    return y
 
-def jumps(dates, jump_list):
-    offset = []
-    for d in dates:
-        OS_value = 0
-        for jump_t, jump_val in jump_list:
-            if d >= jump_t:
-                OS_value += jump_val
-        offset.append(OS_value)
-    return offset
-
-def model(linear_model, harmonic_model, noise, offset):
-    model = []
-    for i in range (len(linear_model)):
-        model.append(linear_model[i] + harmonic_model[i] + offset[i] + noise[i])
-    return model
+##-------------------------------------------------------------------------------------------------------------------------------------------------------------------##
+##----------------------------------------------------------------LEAST SQUARE SOLUTIONS-----------------------------------------------------------------------------##
+##-------------------------------------------------------------------------------------------------------------------------------------------------------------------##
 ## Generating a design matrix based on the given time differences (each time instance (epoch) - the middle epoch), and 2 or more given angular frequencies (omega1,2):
 ## The resulting design matrix named "A" has dimensions "(len(dates), number of coefficients(6))"
 ## The equation being modeled is: y = a*dates + b + c1*math.sin(w1*dates) + d1*math.cos(w1*dates) + c2*math.sin(w2*dates) + d2*math.cos(w2*dates)...
-def Design_Matrix(dates, dt, jump_list, angular_freq):
+def Design_Matrix(t, jump_list, angular_freq):
     A = []
     m = 2 + len(angular_freq) * 2 + len(jump_list)                                #Number of parameters 
-    for d in zip(dates, dt):
+    dt = fractionaldt(t)   
+    for d in zip(t, dt):
         A_row = []                                                              #Creating a row containing the values of the independent variables for each date
         A_row += [d[1], 1]                                                      #Adding the linear terms
         for w in angular_freq:
-            A_row += [math.sin(w * d[1]), math.cos(w * d[1])]           #Adding as many harmonic terms as in the omega list
-        for jump in jump_list:
-            if d[0] >= jump[0]:
+            A_row += [math.sin(w * d[1]), math.cos(w * d[1])]                   #Adding as many harmonic terms as in the omega list
+        for jump_t in jump_list:
+            if d[0] >= jump_t:
                 A_row += [1]
             else:
                 A_row += [0]
@@ -119,79 +197,106 @@ def Design_Matrix(dates, dt, jump_list, angular_freq):
 
 ## Solving a linear regression problem using the least square method
 ## Converting the "y" input list to a NumPy array and reshape it into a column vector ensuring that "y" is in the correct format for further computations
-def Least_Squares(A, model):
-    model = np.array(model)
-    model = np.reshape(model, (len(model),1))
+def fit(y, t, P_matrix, freq, jumps, SIGMA0): ## t = dates in datetime, y = parsed solutions, freq = given angular frequencies, jumps = list of datetime objects (time instance when the jump happened)
+    model = np.reshape(y, (len(y),1))
+    A = Design_Matrix(t, jumps, freq)
+    AP = P_matrix@A
+    dx = np.linalg.lstsq(AP, model, rcond=None)[0].flatten()
+    AT = np.transpose(AP)
+    cov_matrix = np.linalg.inv(AT@AP)*SIGMA0**2
+    return dx, cov_matrix
 
-    dx = np.linalg.lstsq(A, model, rcond=None)[0]
-    return dx
+def residuals(y, y_predicted):
+    residuals = [actual - pred for actual, pred in zip(y, y_predicted)]
+    squared_residuals = [residual ** 2 for residual in residuals]
+    sum_sq_res = sum(squared_residuals)
+    mse = sum_sq_res / (len(y) - 1) ##mean square error
+    return residuals, mse
 
-## Given the least square solutions for an equation: y = a*dates + b + c1*math.sin(w1*dates) + d1*math.cos(w1*dates) + c2*math.sin(w2*dates) + d2*math.cos(w2*dates) + ... + j1 + ...jν
-## For each date, calculate the corresponding solution creating the final model 
-def LS_solutions(dx, dates, dt, angular_freq, jump_list):
-    dx = list(dx)
-    solutions = []   
+def remove_outliers(y, t, P, residuals, sigmap , SIGMA0):
+    limit3s = 3 * sigmap 
+    yy, tt, pp = [], [], []
+    for i, res in enumerate(residuals):
+        if abs(res) < limit3s:
+            tt.append(t[i])
+            yy.append(y[i])
+            pp.append(P[i][i])
+    P = np.array(pp)
+    P_matrix = np.diag(P)
+    return yy, tt, P_matrix
+
+if __name__ == "__main__":
+    dates = date_calc(timeframe)
+
+    t, stime, coords = parse(PATH)
+    x, y, z = getxyz(coords)
+    sof, sol, soh = geterrors(coords)
+    e, n, u = topocentric_conversion(x, y, z)
+    na = n    
+    ta = t
+
+    P_f = weights_calc(sof, SIGMA0)
+
+    # # Validation Tests - Done
+    # #y_arx = compute_model(model_coef, dates, angular_freq, jump_list, white_noise_parameters)
+
+    # Timeseries analysis of X - North axis:
+    j = 0
+    mse=1
+    mset=1000
+    while abs(mse - mset) > 1.0e-6 and j < 10:
+        if j!=0:
+            mset = mse
+        dx, vx = fit(n, t, P_f, angular_freq, jump_list, SIGMA0)
+        y_tel = compute_model(dx, t, angular_freq, jump_list)
+        uu, mse = residuals(n, y_tel)
+        tres = t
+        n, t, P_f = remove_outliers(n, t, P_f, uu, math.sqrt(mse), SIGMA0)
+        print(P_f.shape)
+        #SIGMA0=math.sqrt(mse)
+        j+=1
+
+    print(j)
+    plt.plot(ta, na, 'o', label = "East_Topo_Data", markersize=1.5)
+    plt.plot(t, n, 'o', label = "East_Topo_Data", markersize=1.5)
+
+    # plt.plot(tres, uu, 'o', label = "East_Topo_Data", markersize=1.5)
     
-    for d in zip(dates, dt):
-        lin_sol = dx[0]*d[1] + dx[1]
-        harmonic = 0.0
-        offset = 2
-        for j in range(len(angular_freq)):
-            harmonic += dx[offset+j] * math.sin(angular_freq[j] * d[1]) + dx[offset+1+j] * math.cos(angular_freq[j] * d[1])
-        jumps = 0.0
-        offset = offset + len(angular_freq)*2
-        for j in range(len(jump_list)):
-            if d[0] >= jump_list[j][0]:
-                jumps += dx[offset + j]
-        solution = lin_sol + harmonic + jumps
-        solutions.append(solution)
-    # print(solutions)
+    # plt.plot(t, filtered_y, label = "Linear Regression")
 
-    return solutions
+    font = {'family':'serif','color':'blue','size':30}
+    font1 = {'family':'serif','color':'darkred','size':20}
 
-def lin_sol(dx, dt):
-    dx = list(dx)
-    solutions = []
-    for d in dt:
-        l_sol = dx[0] * d + dx[1]
-        solutions.append(l_sol)
-    return solutions
+    plt.title('Time Series - East', fontdict = font)
+    plt.xlabel('Dates', fontdict = font1)
+    plt.ylabel('y - values (mm)', fontdict = font1)
+    plt.legend()
 
-def harm_sol(dx, angular_freq, dt):
-    dx = list(dx)
-    harm = []
-    for d in dt:
-        harm2 = 0
-        for i, freq in enumerate(angular_freq):
-            harm2 += dx[i*2+2] * math.sin(freq * d) + dx[i*2+3] * math.cos(freq * d)
-        harm.append(harm2)
-    return harm
+    plt.show()
 
+    # ## PLOTTING THE SOLUTION
+    # fig, (ax0, ax1, ax2) = plt.subplots(3, 1, sharex=True)
+    # ax0.scatter(t, e, label='East')
+    # ax1.scatter(t, n, label='North')
+    # ax2.scatter(t, u, label='Up')
 
-t0 = middle_epoch(timeframe)
-dates = date_calc(timeframe)
-dt = epochs2dt(datetime2epochs(dates))
-linear_model = linear(dt, model_coef)
-#harmonic_model = harmonic(dt, angular_freq, model_coef)
-harmonic_model = harm_sol(model_coef, angular_freq, dt)
-#offset = jumps(dates, jump_list)
-#noise = white_noise(timeframe, white_noise_parameters)
-dx = Least_Squares(Design_Matrix(dates, dt, jump_list, angular_freq), model(linear(dt, model_coef), harm_sol(model_coef, angular_freq, dt), jumps(dates, jump_list), white_noise(timeframe, white_noise_parameters)))
-solution = LS_solutions(dx, dates, dt, angular_freq, jump_list)
-y = model(linear(dt, model_coef), harm_sol(model_coef, angular_freq, dt), jumps(dates, jump_list), white_noise(timeframe, white_noise_parameters))
+    # font1 = {'family':'serif','color':'blue','size':30}
+    # font2 = {'family':'serif','color':'darkred','size':20}
 
-lin_test = lin_sol(dx, dt)
-harm_test = harm_sol(dx, angular_freq, dt)
+    # # ax0.set_ylim(-0.1, 0.7)
+    # # ax1.set_ylim(-0.5, 0.2)
+    # # ax2.set_ylim(-0.02, 0.02)
+
+    # ax0.set_title('Timeseries Plot', fontdict = font1)
+    # ax2.set_xlabel("Dates", fontdict = font2)
+
+    # ax0.set_ylabel("East (m)", fontdict = font2)
+    # ax1.set_ylabel("North (m)", fontdict = font2)
+    # ax2.set_ylabel("Up (m)", fontdict = font2)
+
+    # plt.show()
 
 
-plt.plot(dates, harm_test, label = 'Model')
-plt.plot(dates, harmonic_model, label = 'White noise')
 
-font = {'family':'serif','color':'blue','size':30}
-font1 = {'family':'serif','color':'darkred','size':20}
-
-plt.title('Model', fontdict = font)
-plt.xlabel('Dates', fontdict = font1)
-plt.ylabel('y - values (mm)', fontdict = font1)
-
-plt.show()
+    for i,x in enumerate(dx):
+        print('{:+10.3f} +- {:10.6f}'.format(x*1000, math.sqrt(vx[i][i])))
